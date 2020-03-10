6b41194c9a1a37bd05422663911a596374da42d7
Terry Chiem <terrychiem@gmail.com>
2020-03-10 16:09:10 -0700

    complete part 1-3

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 console.log("Let's get this party started!");
+
+$("#submit").on("click", searchGif);
+$("#remove").on("click", removeGifs);
+
+const API_KEY = "xAiC2f53HUVIdrR0m96Hi3x7ZkvNUq9d";
+
+async function searchGif(evt) {
+  evt.preventDefault();
+  let q = $("#search").val();
+  let result = await axios.get("https://api.giphy.com/v1/gifs/search", {
+    params: { api_key: API_KEY, q } 
+  })
+  let url = result.data.data[0].images.downsized.url;
+  let gif = $('<img>');
+  gif.attr('src', url);
+  $('#gifs').append(gif);
+}
+
+function removeGifs(evt){
+  $('#gifs').empty();
+}
+
+// API = xAiC2f53HUVIdrR0m96Hi3x7ZkvNUq9d
